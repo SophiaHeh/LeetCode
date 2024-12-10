f898a1e6ecc9b9d8ad2b125be3fc03a68f21ab3b
SophiaHeh <167366412+SophiaHeh@users.noreply.github.com>
2024-12-09 23:21:51 -0800

    Add files via upload

---
+++ lecture_part3_strings.py
@@ -0,0 +1,22 @@
+'''
+Defining Strings
+'''
+
+word1 = "Wonder land"
+word2 = "Love story"
+
+print(word1  + word2)
+print(word1 + " " + word2)
+
+#Note: \ is a symbol for cancling 
+sentence  = "Let's code together"
+quote = "\"Goodmorning\""
+
+print(quote)
+print("\\" + quote)
+
+# end positon is exclusive
+print(word1[0:4])
+print(word2[5:10]) # same as print(word2[5:])
+
+
