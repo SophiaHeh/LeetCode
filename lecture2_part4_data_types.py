37cb55cc107f75b9a80afc8fda55b410aa0467e9
SophiaHeh <167366412+SophiaHeh@users.noreply.github.com>
2024-12-09 23:22:41 -0800

    Add files via upload

---
+++ lecture2_part4_data_types.py
@@ -0,0 +1,13 @@
+'''
+Exploring different data types
+'''
+
+num_dec = 2.3567
+num_int = 5
+print(type(num_dec))
+print(type(num_int))
+
+
+#note: 
+#* order: not to matter
+#* duplicates: do not want
