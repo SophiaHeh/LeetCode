1e7f4a790c6c7284dac9e332adc01394589c1e5d
SophiaHeh <167366412+SophiaHeh@users.noreply.github.com>
2024-12-15 23:47:28 -0800

    Add files via upload

---
+++ test_car_prac.py
@@ -0,0 +1,12 @@
+import unittest
+from car import Car
+
+class TestCar(unittest.TestCase):
+    '''
+    Design to test the class Car
+    '''
+    def setUp(self): #類似於__init__函數
+        '''
+        Set up
+        '''
+        
